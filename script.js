5bb6dd13fddbcf7218cad1659028595a03be77d2
Leonardo Marino <leomarino867@gmail.com>
2025-06-03 15:28:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ btnNao.addEventListener('mouseover', () => {
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
-    // Mover o botão para uma
+    // Mover o botão
     btnNao.style.position = 'absolute';
     btnNao.style.left = `${randomX}px`;
     btnNao.style.top = `${randomY}px`;
